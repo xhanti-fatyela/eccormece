41f82340464c21b49e80d8bf078c042603ea40aa
xhanti-fatyela <xmanmrcool@gmail.com>
2022-06-03 14:36:13 +0200

    fixed bugs in cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -12,10 +12,11 @@ import { Sneaker } from '../../sneakers';
 export class CartComponent implements OnInit {
   
   sneakers?: Sneaker;
-  items :any
+  // items :any
   cart = JSON.parse(`${localStorage.getItem('cart')}`) ? JSON.parse(`${localStorage.getItem('cart')}`) : []
-  value: number = 1
-  total: number = 0
+ 
+  // value: number = 1
+  total =  0
   constructor(
     private cartService: CartService
   ) { }
@@ -23,6 +24,7 @@ export class CartComponent implements OnInit {
   ngOnInit(): void {
     this.totalPrice()
   //  this.getItems()
+  
 
   }
 
@@ -30,41 +32,69 @@ export class CartComponent implements OnInit {
   //   this.items = this.cartService.getItems()
   // }
 
-  totalPrice(){
-    
-    this.cart.forEach((element: { price: number, quantity: number }) => {
-      this.total += element.price*element.quantity
-    });
-  }
+   deleteItem(i: number): void{
 
-  
+    this.cart.splice(i, 1)
+    this.totalPrice()
+
+   }
+
+  totalPrice(){
+    this.cart.forEach((element: any) => {
+      this.total += element.price*element.quantity
+      
+    });
+    console.log(this.total)
+  }
 
   increment(index: number){
-  
-    console.log(this.value)
-    let q = this.cart[index].quantity++
-    let sneaker = {
-      quantity: q,
-      ...this.cart[index]
-    }
-    this.cart.splice(index,1,sneaker)
-    localStorage.setItem('cart', JSON.stringify(this.cart))
-     console.log(this.cart)
-     this.totalPrice()
+
+    ++this.cart[index].quantity
+    this.cart.splice(index, 1, this.cart[index])
+    console.log(this.cart[index])
+    this.total = 0
+    this.totalPrice()
+    console.log(this.cart)
   }
 
-  decrement(index: number){
-    this.value--
-    console.log(this.value)
-    let sneaker = {
-      quantity: this.value,
-      ...this.cart[index]
-    }
-    this.cart.splice(index,1,sneaker)
-    localStorage.setItem('cart', JSON.stringify(this.cart))
-     console.log(this.cart)
-     this.totalPrice()
+  
+  decrement(quantity: any, index: number){
+    const items = JSON.parse(`${localStorage.getItem('cart')}`) ? JSON.parse(`${localStorage.getItem('cart')}`) : []
+  if(quantity > 1)
+    quantity--
+    items[index].quantity = quantity
+    this.totalPrice()
+   
   }
+
+
+
+  // increment(index: number){
+  
+  //   console.log(this.value)
+  //   let q = this.cart[index].quantity++
+  //   let sneaker = {
+  //     quantity: q,
+  //     ...this.cart[index]
+  //   }
+  //   this.cart.splice(index,1,sneaker)
+  //   localStorage.setItem('cart', JSON.stringify(this.cart))
+  //    console.log(this.cart)
+  //    this.totalPrice()
+  // }
+
+  // decrement(index: number){
+  //   this.value--
+  //   console.log(this.value)
+  //   let sneaker = {
+  //     quantity: this.value,
+  //     ...this.cart[index]
+  //   }
+  //   this.cart.splice(index,1,sneaker)
+  //   localStorage.setItem('cart', JSON.stringify(this.cart))
+  //    console.log(this.cart)
+  //    this.totalPrice()
+  // }
 
  
   
